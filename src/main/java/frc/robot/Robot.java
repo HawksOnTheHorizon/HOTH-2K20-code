dd4c0e22f47a47412fb98a3546a3dabf1c4a8d1a
General00091 <isabelabarca21@hsamckinley.org>
2020-03-01 12:00:29 -0600

    new master code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,27 +7,24 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-//import com.revrobotics.ColorMatch;
-//import com.revrobotics.ColorMatchResult;
-//import com.revrobotics.ColorSensorV3;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.TimedCommand;
-import edu.wpi.first.wpilibj.command.WaitCommand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -37,75 +34,62 @@ import edu.wpi.first.wpilibj.util.Color;
  * project.
  */
 public class Robot extends TimedRobot {
-  
-  //Joysticks 
-  Joystick jStick = new Joystick(0);
-  Joystick jStick2 = new Joystick(1);
+Joystick jStick = new Joystick(0);
+Joystick jStick2 = new Joystick(1);
+JoystickButton lefttrigger = new JoystickButton(jStick2, 8);
+JoystickButton righttrigger = new JoystickButton(jStick2, 7);
+JoystickButton y = new JoystickButton(jStick2, 4);
+JoystickButton a = new JoystickButton(jStick2, 2);
 
-  //Joystick buttons for Driver 2
-  JoystickButton leftTrigger = new JoystickButton(jStick2, 8);
-  JoystickButton rightTrigger = new JoystickButton(jStick2, 7);
-  JoystickButton y = new JoystickButton(jStick2, 4);
-  JoystickButton a = new JoystickButton(jStick2, 2);
-  JoystickButton rtTrigger = new JoystickButton(jStick2, 8);
+JoystickButton xDriver1 = new JoystickButton(jStick, 1);
+JoystickButton bDriver1 = new JoystickButton(jStick, 3);
+JoystickButton leftButton = new JoystickButton(jStick,5);
+JoystickButton rightButton = new JoystickButton(jStick,6);
+JoystickButton ltTrigger = new JoystickButton(jStick,7);
+JoystickButton rtTrigger = new JoystickButton(jStick, 8);
 
-//Joystick buttons for Driver 1
-  JoystickButton x = new JoystickButton(jStick, 1);
-  JoystickButton b = new JoystickButton(jStick, 3);
-  JoystickButton leftButton = new JoystickButton(jStick,5);
-  JoystickButton rightButton = new JoystickButton(jStick,6);
-  JoystickButton ltTrigger = new JoystickButton(jStick,7);
+WPI_TalonSRX intake = new WPI_TalonSRX(6);
+WPI_TalonSRX belt = new WPI_TalonSRX(7);
+WPI_VictorSPX shooter = new WPI_VictorSPX(8);
+WPI_VictorSPX hanger = new WPI_VictorSPX(10);
 
-//Motor controllers for specific mechanisms 
-  WPI_TalonSRX intake = new WPI_TalonSRX(6);
-  WPI_TalonSRX belt = new WPI_TalonSRX(7);
-  WPI_VictorSPX shooter = new WPI_VictorSPX(8);
-  WPI_VictorSPX hanger = new WPI_VictorSPX(9);
-  WPI_VictorSPX controlPanelMech = new WPI_VictorSPX(10);
+WPI_VictorSPX m_frontLeft = new WPI_VictorSPX(5);
+WPI_VictorSPX m_backLeft = new WPI_VictorSPX(3);
+SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_backLeft);
 
-  //Motor controllers for drivetrain (left side) 
-  WPI_VictorSPX m_frontLeft = new WPI_VictorSPX(5);
-  WPI_VictorSPX m_backLeft = new WPI_VictorSPX(3);
-  SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_backLeft);
- 
-  //Motor controllers for drivetrain (right side)
-  WPI_VictorSPX m_frontRight = new WPI_VictorSPX(4);
-  WPI_VictorSPX m_backRight = new WPI_VictorSPX(2);
-  SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_backRight);
-  DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
+WPI_VictorSPX m_frontRight = new WPI_VictorSPX(4);
+WPI_VictorSPX m_backRight = new WPI_VictorSPX(2);
+SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_backRight);
+DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
 
-  //Extras 
-  Timer m_timer = new Timer();
-  ADXRS450_Gyro gyro =new ADXRS450_Gyro();
-  private static final double gyroAngleSetpoint = 0.0;
-  private static final double gyroP = 0.005;
-  private static final SPI.Port gyroPort = SPI.Port.kOnboardCS0;
+Timer m_timer = new Timer();
 
-  //I2C.Port i2cPort = I2C.Port.kOnboard; //adressing I2C port  
-  //ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort); //adressing color sensor and placing 
-  
-  /*ColorMatch m_colorMatcher = new ColorMatch();
-  Color blueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-  Color greenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-  Color redTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-  Color yellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
-  */
+//I2C.Port i2cPort = I2C.Port.kOnboard; //adressing I2C port  
+//ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort); //adressing color sensor and placing 
+ColorMatch m_colorMatcher = new ColorMatch();
+Color blueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+Color greenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+Color redTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+Color yellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
-  //PowerDistributionPanel pdp1 = new PowerDistributionPanel(1);
-  
+//PowerDistributionPanel pdp1 = new PowerDistributionPanel(1);
+
+
+
+
+
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
   @Override
   public void robotInit() {
-    /*m_colorMatcher.addColorMatch(blueTarget);
-    m_colorMatcher.addColorMatch(greenTarget);
-    m_colorMatcher.addColorMatch(redTarget);
-    m_colorMatcher.addColorMatch(yellowTarget);
-   */
-
-    gyro.calibrate();
+  m_colorMatcher.addColorMatch(blueTarget);
+  m_colorMatcher.addColorMatch(greenTarget);
+  m_colorMatcher.addColorMatch(redTarget);
+  m_colorMatcher.addColorMatch(yellowTarget);
+  
   }
 
   /**
@@ -118,40 +102,40 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-     /* Color detectedColor = m_colorSensor.getColor();
-   
-    String colorString;
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
-    
-    if (match.color == blueTarget) {
-      colorString = "Blue";
-    } else if (match.color == redTarget) {
-      colorString = "Red";
-    } else if (match.color == greenTarget) {
-      colorString = "Green";
-    } else if (match.color == yellowTarget) {
-      colorString = "Yellow";
-    } else {
-      colorString = "Unknown";
-    }
-   
-    double IR = m_colorSensor.getIR();
-   
-    SmartDashboard.putNumber("Red", detectedColor.red);
-    SmartDashboard.putNumber("Green", detectedColor.green);
-    SmartDashboard.putNumber("Blue", detectedColor.blue);
-    SmartDashboard.putNumber("Confidence", match.confidence);
-    SmartDashboard.putString("Detected Color", colorString);
-    SmartDashboard.putNumber("IR", IR);
-   
-    SmartDashboard.putNumber("Total Current", pdp1.getTotalCurrent());
-   
-    /*SmartDashboard.putNumber("Right Bottom Motor", pdp1.getCurrent(15));
-    SmartDashboard.putNumber("Right Top Motor", pdp1.getCurrent(14));
-    SmartDashboard.putNumber("Left Bottom Motor", pdp1.getCurrent(13));
-    SmartDashboard.putNumber("Left Top Motor", pdp1.getCurrent(12));
-  */
+  
+ /* Color detectedColor = m_colorSensor.getColor();
 
+  String colorString;
+  ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+  
+  if (match.color == blueTarget) {
+    colorString = "Blue";
+  } else if (match.color == redTarget) {
+    colorString = "Red";
+  } else if (match.color == greenTarget) {
+    colorString = "Green";
+  } else if (match.color == yellowTarget) {
+    colorString = "Yellow";
+  } else {
+    colorString = "Unknown";
+  }
+
+  double IR = m_colorSensor.getIR();
+
+  SmartDashboard.putNumber("Red", detectedColor.red);
+  SmartDashboard.putNumber("Green", detectedColor.green);
+  SmartDashboard.putNumber("Blue", detectedColor.blue);
+  SmartDashboard.putNumber("Confidence", match.confidence);
+  SmartDashboard.putString("Detected Color", colorString);
+  SmartDashboard.putNumber("IR", IR);
+
+  SmartDashboard.putNumber("Total Current", pdp1.getTotalCurrent());
+
+  /*SmartDashboard.putNumber("Right Bottom Motor", pdp1.getCurrent(15));
+  SmartDashboard.putNumber("Right Top Motor", pdp1.getCurrent(14));
+  SmartDashboard.putNumber("Left Bottom Motor", pdp1.getCurrent(13));
+  SmartDashboard.putNumber("Left Top Motor", pdp1.getCurrent(12));
+*/
   }
 
   /**
@@ -178,26 +162,22 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     if (m_timer.get() < 3.5) { 
       System.out.println ("passed stage one");
-      m_drive.tankDrive(-0.5, -0.5);
-   
+      m_drive.tankDrive(-0.75, -0.75);
+ 
     } else {
       m_drive.stopMotor();
     }
-   
-    if ((m_timer.get() > 3.5) && (m_timer.get() < 5)) {
-      belt.set(-1);  
-      shooter.set(-1);
+ 
+    if ((m_timer.get() > 3.5) && (m_timer.get() < 6.5)) {
+    System.out.println ("passed stage two");
+     belt.set(-1);  
+     shooter.set(-1);
     } else {
+      System.out.println ("that's all folks!");
       belt.set(0);
       shooter.set(0);
     }
-  }
 
-  /**
-   * This function is called once when teleop is enabled.
-   */
-  @Override
-  public void teleopInit() {
   }
 
   /**
@@ -206,87 +186,74 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    m_drive.tankDrive(-jStick.getRawAxis(1)*0.75, -jStick.getRawAxis(3)*0.75);
+  m_drive.tankDrive(-jStick.getRawAxis(1)*0.75, -jStick.getRawAxis(3)*0.75);
   
-    if (rightButton.get()) {
-      m_drive.tankDrive(jStick.getRawAxis(1)*0.75, jStick.getRawAxis(3)*0.75); //reverse drivetrain; shooter becomes forward
-    }
+  if (rightButton.get()) {
+    m_drive.tankDrive(jStick.getRawAxis(1)*0.75, jStick.getRawAxis(3)*0.75); //reverse drivetrain; shooter becomes forward
+  } 
+
+  /*if (x.get()) { //reverse intake
+    intake.set(1);
+    belt.set(1);
+  } else if (b.get()) { //intake 
+    intake.set(-1);
+    belt.set(-1); 
+  } else {
+    intake.set(0);
+    belt.set(0);
+  }
+  */
+
+  if (lefttrigger.get()) {
+    belt.set(-1); //belt moves towards shooter
+  } else if (righttrigger.get()) {
+    belt.set(1); //belt moves toward intake 
+  } else {
+    belt.set(0);
+  }
 
 
-    if (leftTrigger.get()) {
-      belt.set(-1); //belt moves towards shooter
-    } else if (rightTrigger.get()) {
-      belt.set(1); //belt moves toward intake 
-    } else {
-      belt.set(0);
-    }
-    
+  if (y.get()) { //shooter; spit balls out
+    shooter.set(-1);
+  } else if (a.get()) { // reverse shooting; intakes ball from loading station 
+    shooter.set(1);
+  } else {
+    shooter.set(0);
+  }
 
-      /*if (y.get()) {
-        intake.set(-1); for 0.5 seconds
-        while shooter.set(1) for 5 seconds 
-              intake.set(1) will start running for 5 seconds
-        else 
-        intake.set(0);       
-      }
-    */
+  if (leftButton.get()) {
+    intake.set(1);
+  } else if (ltTrigger.get()) {
+    intake.set(-1);
+  } else {
+    intake.set(0);
+  }
 
-
-    if (y.get()) {
-      belt.set(1); 
-      shooter.set(-1);//shooter; spit balls out
-      belt.set(-1);
-    } else if (a.get()) { // reverse shooting; intakes ball from loading station 
-      belt.set(-1);
-      shooter.set(1);
-      belt.set(1);
-    } else {
-      belt.set(0);
-      shooter.set(0);
-      belt.set(0);
-    }
-  
-
-    if (leftButton.get()) {
-      intake.set(0);
-    } else if (ltTrigger.get()) {
-      intake.set(-1);
-    } else {
-      intake.set(0);
-    }
-
-
-    if (x.get()) {
+  if (xDriver1.get()) {
     // control panel (rotation control)
-    } else if (b.get()) {
+  } else if (bDriver1.get()) {
     // control panel (position control)
-    } else {
+  } else {
     // nothing happens 
-    }
-    
   }
 
-
-  /**
-   * This function is called once when the robot is disabled.
-   */
-  @Override
-  public void disabledInit() {
+  if (rtTrigger.get()) {
+    hanger.set(0.25);// hanger goes up
+  } else {
+    hanger.set(0);// nothing happens
   }
 
-  /**
-   * This function is called periodically when disabled.
-   */
-  @Override
-  public void disabledPeriodic() {
+  /*if (leftTrigger.get()) {
+    belt.set(1);
+  } else if (rightTrigger.get()) {
+    belt.set(-1);
+  } else {
+    belt.set(0);
   }
 
-  /**
-   * This function is called once when test mode is enabled.
-   */
-  @Override
-  public void testInit() {
-  }
+*/ 
+
+}
 
   /**
    * This function is called periodically during test mode.
@@ -294,5 +261,4 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {
   }
-  
 }
